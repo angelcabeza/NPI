c260607e8f9ceb856272ef08e3b75cf11a4d4981
angelcabeza <angelcabezamartin@gmail.com>
2021-10-26 15:12:32 +0200

    Ya funciona el guiar a un alumno, faltan mas rutas y el gesto de perdido

--- AppAndroid/app/src/main/java/com/example/ugr_ubicate/clasesActivity.kt
+++ AppAndroid/app/src/main/java/com/example/ugr_ubicate/clasesActivity.kt
@@ -13,6 +13,7 @@ import android.widget.TextView
 import android.hardware.SensorEventListener
 import android.os.Build
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
@@ -20,13 +21,23 @@ import androidx.core.content.ContextCompat
 
 class clasesActivity : AppCompatActivity(), SensorEventListener {
 
-    private lateinit var sensorManager: SensorManager
-    private lateinit var sensor: Sensor
-    private var running = false
+    private lateinit var sensorManagerPodometro: SensorManager
+    private lateinit var sensorManagerBrujula: SensorManager
+    private lateinit var sensorPodometro: Sensor
+    private lateinit var sensorBrujula: Sensor
     private var previousTotalSteps = 0f
     private var totalSteps = 0f
+    private var currentSteps = 0
+    private var referencia = false
+    private var referenciaGiro = 0f
+    private var giro = 0f
     private var instruccionesRuta1: Array<String> = arrayOf("Salga por la puerta de la clase", "Gire a la derecha", "Camine recto hasta encontrar la clase 3.6")
-    private lateinit var  titulo: TextView
+    private lateinit var  instrucciones: TextView
+    private lateinit var  debug: TextView
+    private lateinit var  textGiro: TextView
+    private var primeraInstruccion = false
+    private var segundaInstruccion = false
+    private var terceraInstruccion = false
     private var cont = 0
     val ACTIVITY_RQ = 101
 
@@ -34,37 +45,81 @@ class clasesActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clases)
 
-        titulo = findViewById(R.id.instruccionesContainer)
+        instrucciones = findViewById(R.id.instruccionesContainer)
+        debug = findViewById(R.id.debug)
+        textGiro = findViewById(R.id.textGiro)
+        instrucciones.visibility = View.INVISIBLE
+
+        val titulo = findViewById<TextView>(R.id.titulo)
+        val ruta1 = findViewById<Button>(R.id.ruta1)
+        ruta1.setOnClickListener{
+            titulo.visibility = View.INVISIBLE
+            ruta1.visibility = View.INVISIBLE
+            instrucciones.text = instruccionesRuta1[cont]
+            instrucciones.visibility = View.VISIBLE
+            sensorPodometro = sensorManagerPodometro.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+            sensorManagerPodometro?.registerListener(this, sensorPodometro, SensorManager.SENSOR_DELAY_FASTEST)
+
+            sensorBrujula = sensorManagerBrujula.getDefaultSensor((Sensor.TYPE_ORIENTATION))
+            sensorManagerBrujula?.registerListener(this, sensorBrujula, SensorManager.SENSOR_DELAY_NORMAL)
+        }
 
         checkForPermission(android.Manifest.permission.ACTIVITY_RECOGNITION,"activity",ACTIVITY_RQ)
+
         // Declaro el podómetro
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        sensor= sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-
-        titulo.text = totalSteps.toString()
+        sensorManagerPodometro = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManagerBrujula = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
-        totalSteps= p0!!.values[0]
-        titulo.text = totalSteps.toString()
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.sensor.type == Sensor.TYPE_STEP_DETECTOR){
+            currentSteps +=1
+            debug.text = currentSteps.toString()
+        }
+
+        if (event!!.sensor.type == Sensor.TYPE_ORIENTATION && primeraInstruccion && !segundaInstruccion) {
+            if (!referencia && primeraInstruccion) {
+                referenciaGiro = event!!.values[0]
+                referencia = true
+            }
+            giro = referenciaGiro - event!!.values[0]
+            textGiro.text = giro.toString()
+        }
+
+        ruta1()
     }
 
-    override fun onResume(){
-        super.onResume()
-        sensorManager.registerListener(this,sensor,1)
-    }
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
 
+
+    override fun onStop() {
+        super.onStop()
+        sensorManagerPodometro.unregisterListener(this,sensorPodometro)
+        sensorManagerBrujula.unregisterListener(this,sensorBrujula)
+    }
+
+
     fun ruta1(){
-        if (totalSteps.toInt() != 0 && totalSteps > 15 ){
+        if (currentSteps >= 10 && !primeraInstruccion){
             cont++
-            titulo.text = instruccionesRuta1[cont]
-            titulo.visibility = View.VISIBLE
+            instrucciones.text = instruccionesRuta1[cont]
+            primeraInstruccion = true
+            currentSteps = 0
+        }
+        else if (primeraInstruccion && giro <= -80 && giro >= -110 && !segundaInstruccion) {
+            cont++
+            instrucciones.text = instruccionesRuta1[cont]
+            segundaInstruccion = true
+        }
+        else if (primeraInstruccion && segundaInstruccion && currentSteps >= 5){
+            instrucciones.text = "¡Ha llegado a su destino!"
+            terceraInstruccion = true
         }
     }
 
+
+    // MANAGE PERMISSION
     private fun checkForPermission(permission: String, name: String, requestCode: Int) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             when {
