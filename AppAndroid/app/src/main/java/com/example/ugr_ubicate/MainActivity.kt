23d902f3be8c34552a5fcfa6acf8681dca9ab5eb
Toni <romanbellics@gmail.com>
2021-11-07 11:22:54 +0100

    [Bug] buttons and vertical sensivility

--- AppAndroid/app/src/main/java/com/example/ugr_ubicate/MainActivity.kt
+++ AppAndroid/app/src/main/java/com/example/ugr_ubicate/MainActivity.kt
@@ -9,13 +9,14 @@ import android.view.InputDevice
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.app.ActivityCompat
 import androidx.core.view.MotionEventCompat
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     var lastx = 0
-    var lasty = 0
+    var lasty = 9999
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +48,12 @@ class MainActivity : AppCompatActivity() {
         xPos = MotionEventCompat.getX(event, index).toInt()
         yPos = MotionEventCompat.getY(event, index).toInt()
 
+
+        if (event.action == 3 && event.pointerCount == 3) {
+            checkVerticalSlice(event, xPos, yPos)
+        }
+
+
         if (event.action == MotionEvent.ACTION_MOVE && event.pointerCount == 3) {
             checkSlice(event, xPos, yPos)
         }
@@ -54,8 +61,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkSlice(event: MotionEvent, xPos: Int, yPos: Int): Boolean{
+        Log.d("DEBUG", "MOTION EVENT. ${event.action}")
+        Log.d("DEBUG", "EJE Y: $yPos")
         if (event.source and InputDevice.SOURCE_CLASS_POINTER != 0) {
-
+            Log.d("DEBUG", "EJE Y: $yPos")
             if (xPos > lastx + 300) {
                 lastx = xPos
                 val intent = Intent(this, clasesActivity::class.java)
@@ -66,7 +75,16 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this, MapsActivity::class.java)
                 startActivity(intent)
                 return true
-            } else if (yPos < lasty - 300) {
+            }
+        }
+        return true
+    }
+
+    fun checkVerticalSlice(event: MotionEvent, xPos: Int, yPos: Int): Boolean{
+        Log.d("DEBUGasdasdasd", "MOTION EVENT. ${event.action}")
+        Log.d("DEBUGasdasdasd", "EJE Y: $yPos")
+        if (event.source and InputDevice.SOURCE_CLASS_POINTER != 0) {
+            if (yPos < lasty - 300) {
                 lasty = yPos
                 val intent = Intent(this, TimeTableActivity::class.java)
                 startActivity(intent)
