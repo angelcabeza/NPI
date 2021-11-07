9b480a7d6473952c8b5ef0ede03178e10c8ed9ea
AngieFloro <93588713+AngieFloro@users.noreply.github.com>
2021-11-07 11:42:16 +0100

    Added motion feature, added timetable view, linked to classesActivity… (#1)
    * Added motion feature, added timetable view, linked to classesActivity, thmbnail updated. REMINDER: build configuration has been changed cause of my personal device, feel free to change to your own, touchActiivity is deprecated, feel free to remove too.
    
    * [Bug] Fixed some button size [Feat] Remove touch class [Feat] change vertical motion direction
    
    * [Bug] buttons and vertical sensivility
    
    Co-authored-by: Toni <romanbellics@gmail.com>

--- AppAndroid/app/src/main/java/com/example/ugr_ubicate/MainActivity.kt
+++ AppAndroid/app/src/main/java/com/example/ugr_ubicate/MainActivity.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.InputDevice
+import android.view.MotionEvent
 import android.widget.Button
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.app.ActivityCompat
+import androidx.core.view.MotionEventCompat
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    var lastx = 0
+    var lasty = 9999
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +37,61 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
+
     }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val index = MotionEventCompat.getActionIndex(event)
+        var xPos = -1
+        var yPos = -1
+
+        xPos = MotionEventCompat.getX(event, index).toInt()
+        yPos = MotionEventCompat.getY(event, index).toInt()
+
+
+        if (event.action == 3 && event.pointerCount == 3) {
+            checkVerticalSlice(event, xPos, yPos)
+        }
+
+
+        if (event.action == MotionEvent.ACTION_MOVE && event.pointerCount == 3) {
+            checkSlice(event, xPos, yPos)
+        }
+        return true
+    }
+
+    fun checkSlice(event: MotionEvent, xPos: Int, yPos: Int): Boolean{
+        Log.d("DEBUG", "MOTION EVENT. ${event.action}")
+        Log.d("DEBUG", "EJE Y: $yPos")
+        if (event.source and InputDevice.SOURCE_CLASS_POINTER != 0) {
+            Log.d("DEBUG", "EJE Y: $yPos")
+            if (xPos > lastx + 300) {
+                lastx = xPos
+                val intent = Intent(this, clasesActivity::class.java)
+                startActivity(intent)
+                return true
+            } else if (xPos < lastx - 300) {
+                lastx = xPos
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return true
+    }
+
+    fun checkVerticalSlice(event: MotionEvent, xPos: Int, yPos: Int): Boolean{
+        Log.d("DEBUGasdasdasd", "MOTION EVENT. ${event.action}")
+        Log.d("DEBUGasdasdasd", "EJE Y: $yPos")
+        if (event.source and InputDevice.SOURCE_CLASS_POINTER != 0) {
+            if (yPos < lasty - 300) {
+                lasty = yPos
+                val intent = Intent(this, TimeTableActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return true
+    }
+
 }
